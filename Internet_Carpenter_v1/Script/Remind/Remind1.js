ab52303cdc517ff4a97cdf29f3688c96d119cfd6
s99113266 <41054903+s99113266@users.noreply.github.com>
2021-10-22 23:22:47 +0800

    複製JS、CSS至專案

---
+++ Internet_Carpenter_v1/Script/Remind/Remind1.js
@@ -0,0 +1,38 @@
+﻿function RemindEstablish(e, types = 0) { ///產生提醒視窗 RemindEstablish("字串", 數值 0 或 1 = 出現NO的按鈕)
+    let RemindHTML, RemindObj, RemindButtonNO;
+    RemindButtonNO = "";
+    if (types == 1) {
+        RemindButtonNO = `<div class="RemindNO pos_r flexNone">
+                                        <div class="pos_a dpflex jfc_center ai_center c_pointer"><ion-icon name="close"></ion-icon></div>
+                                    </div>`;
+    }
+    RemindHTML = `    <div class="Remind pos_f FDColumn">
+                                    <div class="RemindContent flex_1 FDColumn">
+                                        <div class="RemindText flex_1 FDColumn ovf_auto">
+                                            <div class="flex_1 dpflex jfc_center ai_center ovf_auto">${e}</div>
+                                        </div>
+                                        <div class="RemindButton flexNone FDRow">
+                                            <div class="RemindOK pos_r flex_1">
+                                                <div class="pos_a dpflex jfc_center ai_center c_pointer"><ion-icon name="checkmark"></ion-icon></div>
+                                            </div>
+                                            ${RemindButtonNO}
+                                        </div>
+                                    </div>
+                                </div>`;
+    RemindObj = Doc.createElement("div");
+    RemindObj.classList = "RemindBackground pos_f";
+    RemindObj.innerHTML = RemindHTML;
+    Doc.body.appendChild(RemindObj);
+    return new Promise(function (resolve, reject) {
+        Doc.querySelectorAll(".RemindOK")[0].onclick = function () {
+            RemindObj.remove();
+            resolve(0);
+        }
+        if (types == 1) {
+            Doc.querySelectorAll(".RemindNO")[0].onclick = function () {
+                RemindObj.remove();
+                resolve(1);
+            }
+        }
+    });
+}
---
+++ Internet_Carpenter_v1/Script/basisJS.js
@@ -0,0 +1,106 @@
+﻿const Win = window;
+const Doc = document;
+function getNameValue(GetName) {
+    let nURL = new URL(window.top.location.href);
+    let nGet = new URLSearchParams(nURL.search);
+    return nGet.get(GetName) != null ? nGet.get(GetName).toString() : "";
+
+}
+function getURL(TeUrlFile) {
+    return location.protocol + "//" + location.host + "/" + TeUrlFile;
+}
+function WinTopReplace(teurl) {
+    window.top.location.replace(teurl);
+}
+function UpdateNowUrl(pageName, pageUrl) {
+    window.history.pushState({ "page": pageName }, "", getURL(pageUrl));
+}
+function AjaxPage(tagID, pageTarget) {
+    var xmlHttp, pageDoc;
+    pageTarget = (pageTarget.toLowerCase().indexOf("http") == -1) ? getURL(pageTarget) : pageTarget;
+
+    return new Promise(function (resolve, reject) {
+        if (pageDoc = document.getElementById(tagID)) {  //顯示資料的物件ID
+            if (window.XMLHttpRequest) {
+                //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+                xmlHttp = new XMLHttpRequest();
+            } else {
+                // IE6, IE5 浏览器执行代码
+                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlHttp.onreadystatechange = function () {
+                let ajaxview = "";
+                switch (xmlHttp.status) {
+                    case 200:
+                        if (xmlHttp.readyState == 3) { pageDoc.innerHTML = "Loading..."; }
+                        if (xmlHttp.readyState == 4) { ajaxview = xmlHttp.responseText; }
+                        break;
+                    default:
+                        ajaxview = xmlHttp.status;
+                        break;
+                }
+                if (ajaxview.length > 0) {
+                    pageDoc.innerHTML = ajaxview
+                    resolve(1);
+                } else {
+                    resolve(1);
+                }
+            }
+            xmlHttp.open("POST", pageTarget, true);
+            xmlHttp.send();
+        } else {
+            reject(0);
+        }
+    })
+}
+
+
+//使用Ajax傳送表單
+//AjaxForm(this物件顯示 , 表單ID, 送出路徑, Method模式)
+function AjaxForm(ClientThisBut, ClientFormID, ActionPurpose, FormMethod) {
+    var ClientData = document.getElementById(ClientFormID);
+    return new Promise(function (resolve, reject) { //宣告非同步执行
+        let ButHtml = ClientThisBut.innerHTML; //紀錄按鈕
+        let but = (val) => {
+            if (val == 0) {
+                ButHtml = ClientThisBut.innerHTML; //紀錄按鈕
+                ClientThisBut.disabled = true;
+                ClientThisBut.innerHTML = "Loading...";
+            }
+            if (val == 1) {
+                ClientThisBut.disabled = false;
+                ClientThisBut.innerHTML = ButHtml;
+            }
+        }
+        but(0);
+        ActionPurpose = (ActionPurpose.toLowerCase().indexOf('http') == -1) ? getURL(ActionPurpose) : ActionPurpose;
+        if (ClientData) {
+
+            let XmlHttp = new XMLHttpRequest();
+            XmlHttp.addEventListener("readystatechange", function () {
+                switch (XmlHttp.status) {
+                    case 0:
+                        break;
+                    case 200:
+                        if (XmlHttp.readyState == 4) {
+                            resolve(XmlHttp.responseText);  //完成后发送完成讯息
+                            but(1);
+                        }
+                        break;
+                    default:
+                        reject("0");  //失败发送讯息
+                        break;
+                }
+            });
+            XmlHttp.open(FormMethod, ActionPurpose);
+            XmlHttp.send(new FormData(ClientData));
+        } else {
+            reject("404");
+            but(1);
+        }
+    });
+}
+
+function BodyOverflow(overflowVal) {
+    document.body.style.overflow = (overflowVal != "") ? overflowVal : ""; 
+}
